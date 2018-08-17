4027efc9e255028d1de7b1b6be6df880c1f32b63
Dylan Richards <dylanrichards@Dylans-MacBook-Pro.local>
2018-08-17 19:19:47 -0400

    Change UI to use real data

--- ui.rb
+++ ui.rb
@@ -1,7 +1,7 @@
 require_relative "cashier"
 require_relative "order"
 
-order = Order.new("./test_menu.txt")
+order = Order.new("./menu.txt")
 result = Cashier.(order)
 error_message = "No combinations found for #{order.cash_on_hand}"
 
