7694c7c0cd71e9caf9dc16196edd2a168cb6c9cb
Dylan Richards <dylanrichards@Dylans-MacBook-Pro.local>
2018-08-17 15:00:09 -0400

    Fix error messaging

--- ui.rb
+++ ui.rb
@@ -1,6 +1,14 @@
 require_relative "cashier"
 require_relative "order"
 
-Cashier.(Order.new("./menu.txt")).each do |combination|
-  p combination.map { |combination| "#{combination.name} | #{combination.price}" }
+order = Order.new("./menu.txt")
+result = Cashier.(order)
+error_message = "No combinations found for #{order.cash_on_hand}"
+
+if result == error_message
+  p error_message
+else
+  result.each do |combination|
+    p combination.map { |combination| "#{combination.name} | #{combination.price}" }
+  end
 end
